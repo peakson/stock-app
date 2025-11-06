da3dd9412c2eee43cb610cfcc6103a1b2a0f5d93
peakson <lewis.peakson@gmail.com>
2025-11-06 16:11:07 +0800

    git commit done 2nd.

---
+++ app.js
@@ -0,0 +1,11 @@
+function sayHello(name) {
+  console.log('Hello, ' + name);
+}
+
+sayHello('VS Code');
+
+// --- IGNORE ----  
+function sayNo(name) {
+    console.log('No, ' + name);
+}
+sayNo('VS Code');
